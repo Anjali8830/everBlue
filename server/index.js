7d2d1a87e8475327e12acd06bed991c0a92e5682
Anjali8830 <anjali.as151singh@gmail.com>
2026-01-30 13:16:59 +0530

    index js changes for env

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import transactionRoutes from './routes/transactions.js';
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
